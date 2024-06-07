eefa798326df4f3a0d11d503860b236b94aa8922
Gabrielle Pariag <116374815+GabriellePariag@users.noreply.github.com>
2024-06-07 17:40:41 -0400

    Time: 3 ms (34.45%) | Memory: 41.5 MB (74.32%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,8 +1,11 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         HashMap<Integer, Integer> map = new HashMap<>();
+        // for each num in the arr
         for(int i = 0; i < arr.length; i++) {
+            // add one to the value (occurences) if it exists in the map
             if(map.get(arr[i]) != null) map.put(arr[i], map.get(arr[i])+1);
+            // put in map if absent, and set occurences to 0
             else {
                 map.putIfAbsent(arr[i], 0);
             }     
@@ -10,6 +13,7 @@ class Solution {
 
         Set<Integer> set = new HashSet<>();
         List<Integer> values = new ArrayList<>(map.values());
+        // iterate through all the values and find if they are distinct using a set
         for(int i = 0; i < values.size(); i++) {
             if(!set.add(values.get(i))) return false;
         }
