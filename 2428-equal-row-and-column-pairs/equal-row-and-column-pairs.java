7d7e8ec0205c98d0c4c28928a52bcd73fd0b8883
Gabrielle Pariag <116374815+GabriellePariag@users.noreply.github.com>
2024-06-10 12:37:55 -0400

    Time: 184 ms (5.91%) | Memory: 54.9 MB (11.24%) - LeetSync

---
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int equalPairs(int[][] grid) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        List<Integer> temp = new ArrayList<>();
+        List<Integer> temp2 = new ArrayList<>();
+
+        for(int i = 0; i < grid.length; i++) {
+            // add the column to the temp list
+            for (int j = 0; j < grid.length; j++) {
+                temp2.add(grid[i][j]);
+                temp.add(grid[j][i]);
+            }
+            // map the row to the column
+            map.put(i, new ArrayList<>(temp));
+            temp.clear();
+        }
+
+        for(Map.Entry<Integer, List<Integer>> entry: map.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + " ,Value: " + entry.getValue());
+        }
+
+        // check for equal row and column pairs
+        int result = 0;
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < grid.length; i++) {
+            list = Arrays.stream(grid[i]).boxed().collect(Collectors.toList());
+            int freq = Collections.frequency(map.values(), list);
+            result += freq;
+            /*
+            if(map.containsValue(list)) {
+
+                result++;
+            }
+            */
+            list.clear();
+        }
+
+        return result;
+    }
+}
