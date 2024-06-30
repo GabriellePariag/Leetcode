24261baaa3e72512ba2a4bbbe9922e7ebd87cd7c
Gabrielle Pariag <116374815+GabriellePariag@users.noreply.github.com>
2024-06-30 12:13:49 -0400

    Time: 0 ms (100.00%) | Memory: 39.9 MB (82.81%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,31 +1,23 @@
 class Solution {
     public int climbStairs(int n) {
+        // memoization cache
         ArrayList<Integer> cache = new ArrayList<>();
+        // fill cache will -10 to indicate result has not been stored for given n
         for(int i = 0; i < 46; i++) cache.add(i, -10);
         return findTotal(n, cache);
     }
 
     public int findTotal(int n, ArrayList<Integer> cache) {
+        // if we go below step 0, do not count as a distinct way
         if(n < 0) return 0;
+        // if we reach 0, count as a valid distinct way
         else if(n == 0) return 1;
+        // check cache for n
         else if(cache.get(n) != -10) return cache.get(n);
+
+        // try going down one step, and two steps
         int result = findTotal(n-1, cache) + findTotal(n-2, cache);
         cache.add(n, result);
         return result;
     }
 }
-
-/*
-when n is reached, add one to return val
-climbStairs(n-1);
-climbStairs(n-2);
-
-if (n < 0) return 0;
-if (n == 0) return total++;
-return climbStairs(n-1, total) + climbStairs(n-2, total)
-
-add in memoization
-index 1
-store return value
-
-*/
