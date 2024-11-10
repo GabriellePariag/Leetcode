c458e438d1a96a87da3e68425a3501cae3cd0e8e
Gabrielle Pariag <116374815+GabriellePariag@users.noreply.github.com>
2024-11-09 21:05:20 -0500

    Time: 10 ms (50.76%) | Memory: 46.1 MB (5.45%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,16 +1,52 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int l = 0;
+        int r = nums.length-1;
+        int[] result = new int[2];
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
+            // if the number has been recorded, add the next index in the list
+            if(map.containsKey(nums[i])) {
+                List<Integer> temp = map.get(nums[i]);
+                temp.add(i);
+                map.put(nums[i], temp);
+            }
+            else {
+                List<Integer> list = new ArrayList<>();
+                list.add(i);
+                map.put(nums[i], list);
+            }
+           
         }
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement) && map.get(complement) != i) {
-                return new int[] { i, map.get(complement) };
+
+        Arrays.sort(nums);
+
+        while(l < r) {
+            if (nums[r] + nums[l] == target) {
+                List<Integer> one = map.get(nums[l]);
+                List<Integer> two = map.get(nums[r]);
+                if(nums[r] == nums[l]) {
+                    result[0] = one.get(0);
+                    result[1] = two.get(1);
+                }
+                else {
+                    result[0] = one.get(0);
+                    result[1] = two.get(0);
+                }
+                
+            }
+            if (nums[r] + nums[l] > target) r--;
+            else {
+                l++;
             }
         }
-        // In case there is no solution, return an empty array
-        return new int[] {};
+
+        return result;
     }
 }
+/*
+sort and go in two directions
+2 7 11 15
+2 3 4
+
+*/
