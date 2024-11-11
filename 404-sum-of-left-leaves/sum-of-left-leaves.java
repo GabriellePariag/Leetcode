a41d9fadb052d5f7ddc282bfa9f933936e9e9f99
Gabrielle Pariag <116374815+GabriellePariag@users.noreply.github.com>
2024-11-10 20:39:47 -0500

    Time: 0 ms (100.00%) | Memory: 41.2 MB (66.25%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        return recursive(root);
+    }
+
+    public int recursive(TreeNode root) {
+        int temp = 0;
+        if (root == null) return 0;
+        // if there is a left root
+        if (!(root.left == null)) {
+            // and it's a leaf
+            if ((root.left.left == null) && (root.left.right == null)) temp = root.left.val;
+        }
+        return recursive(root.left) + recursive(root.right) + temp;
+    }
+}
+/*
+
+if this.left is not null, return val to sum
+base case: if root is null, return 0;
+if left and right exist, call the recursive solution with both
+    return recursive(left) + recursive(right) + this.left.val
+
+*/
