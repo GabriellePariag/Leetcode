1ff788586a21346e3051c14213de0fa765ef1087
Gabrielle Pariag <116374815+GabriellePariag@users.noreply.github.com>
2024-11-10 20:15:37 -0500

    Time: 1 ms (100.00%) | Memory: 44 MB (91.89%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -18,12 +18,15 @@ class Solution {
 
         while (!(l1 == null) || !(l2 == null)) {
             ListNode node = new ListNode();
+            // flag checks if we've set our reference (head) for the return result
             while(flag) {
                 ref = node;
                 flag = false;
             }
+            // if we are not on the first node, start setting the arrows
             if(!(prev == null)) prev.next = node;
 
+            // check for any leading linked lists
             if(!(l1 == null) && (l2 == null)){
                 sum = l1.val + carry;
                 l1 = l1.next;
@@ -32,12 +35,14 @@ class Solution {
                 sum = l2.val + carry;
                 l2 = l2.next;
             }
+            // if they both still have two digits, find sum of all digits
             else {
                 sum = l1.val + l2.val + carry;
                 l1 = l1.next;
                 l2 = l2.next;
             }
 
+            // calculate final node value using carry value (like manual addition)
             if(sum < 10) {
                 node.val = sum;
                 carry = 0;
@@ -49,18 +54,19 @@ class Solution {
             else {
                 node.val = sum%10;
                 carry = (10%sum)/10;
-                System.out.println("new node val is: " + node.val);
-                System.out.println("carrying: " + carry);
             }
 
+            // add the node to the list
             prev = node;
         }
 
+        // if there are any remaining digits to be carried, add them to the list
         if (carry > 0) {
             ListNode node = new ListNode(carry);
             if(!(prev == null)) prev.next = node;
         }
 
+        // return the reference head we made earlier
         return ref;
     }
 }
