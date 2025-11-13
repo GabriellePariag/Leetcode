76c9c3759434442b127e99c862e70d93e39413cd
Gabrielle Pariag <116374815+GabriellePariag@users.noreply.github.com>
2025-11-13 17:32:03 -0500

    Time: 0 ms (100.00%) | Memory: 42.1 MB (5.17%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -1,24 +1,29 @@
 class Solution {
     public int kthGrammar(int n, int k) {
         if(n == 1) return 0;
-        int curr = k; // 8
+        int curr = k;
         int result;
         boolean isOpposite = false;
 
-        // i = 2
         for(int i = n; i > 2; i--) {
-            int size = (int)Math.pow(2, i-1);
-            result = transformRow(size, curr);
+            int size = (int)Math.pow(2, i-1); // rows double in size per ea new row
+            result = checkSide(size, curr);
+
+            // if we need to switch sides
             if (result == 0) {
+                // calculate new index on left side that is equiv as right side positioning
                 curr = curr - size/2;
-                System.out.println("row size: " + size + " curr pos: " + curr );
+                // if already flagged, turn it off
                 if(isOpposite) isOpposite = false;
                 else isOpposite = true;
             }
+
+            // otherwise, do nothing. the index is good and we can move up a row
         }
         
+        // once we are on row 2, with just 2 choices, find the matching symbol
         if(curr == 1) {
-            if(isOpposite) return 1;
+            if(isOpposite) return 1; // account for opposite flag
             return 0;
         }
         if(curr == 2) {
@@ -29,7 +34,8 @@ class Solution {
         return 0;
     }
 
-    public int transformRow(int size, int curr) {
+    // checks if index is on the right or left side of the row
+    public int checkSide(int size, int curr) {
         if(curr > size/2) return 0; // 0 means opposite int on left side
         else return 1; // 1 means same int as it currently is
     }
