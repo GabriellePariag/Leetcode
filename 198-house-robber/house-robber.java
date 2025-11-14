36b892db8ad20c445803117fcf4065b8c14cd3ec
Gabrielle Pariag <116374815+GabriellePariag@users.noreply.github.com>
2025-11-13 19:50:19 -0500

    Time: 0 ms (100.00%) | Memory: 43 MB (5.73%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,28 +2,29 @@ class Solution {
     public int rob(int[] nums) {
         /*
         state: which house you are at
+        array stores the max loot at each given state
         */
         int[] dp = new int[nums.length];
-        dp[0] = nums[0];
+        dp[0] = nums[0]; // base case
         
         for(int i = 1; i < nums.length; i++) {
-            if(i == 1) {
+            if(i == 1) { // base case 2
                 dp[1] = nums[1];
                 continue;
             }
-            else if(i == 2) {
+            else if(i == 2) { // base case 3
                 dp[2] = nums[2] + nums[0];
                 continue;
             }
-            dp[i] = Math.max(nums[i] + dp[i-2], nums[i] + dp[i-3]);
+            dp[i] = Math.max(nums[i] + dp[i-2], nums[i] + dp[i-3]); // recurrance relation
         }
 
+        // return final maximum
         int max = 0;
         for(int i = 0; i < dp.length; i++) {
             if(dp[i] > max) max = dp[i];
         }
 
         return max;
-        
     }
 }
